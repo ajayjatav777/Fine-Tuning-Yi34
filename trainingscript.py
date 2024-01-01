791c166464e22b42142af291ccdfdbb13b4f1623
ajayjatav777 <jatav.creative777@gmail.com>
2024-01-01 12:42:28 +0530

    uploading Files

---
+++ trainingscript.py
@@ -0,0 +1,218 @@
+
+# pip install --upgrade pip
+# pip install -q -U bitsandbytes
+# pip install -q -U git+https://github.com/huggingface/transformers.git
+# pip install -q -U git+https://github.com/h
+# uggingface/peft.git
+# !pip install -q -U git+https://github.com/huggingface/accelerate.git
+# !pip install -q -U datasets scipy ipywidgets matplotlib
+
+
+from accelerate import FullyShardedDataParallelPlugin, Accelerator
+from torch.distributed.fsdp.fully_sharded_data_parallel import FullOptimStateDictConfig, FullStateDictConfig
+
+fsdp_plugin = FullyShardedDataParallelPlugin(
+    state_dict_config=FullStateDictConfig(offload_to_cpu=True, rank0_only=False),
+    optim_state_dict_config=FullOptimStateDictConfig(offload_to_cpu=True, rank0_only=False),
+)
+
+accelerator = Accelerator(fsdp_plugin=fsdp_plugin)
+
+
+from datasets import load_dataset
+
+train_dataset, test_dataset = load_dataset('json', data_files='data.jsonl', split=['train[:80%]', 'train[80%:]'])
+
+def formatting_func(example):
+    text = f"### Prompt: {example['prompt']}\n ### Completion: {example['completion']}"
+    return text
+
+
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+
+base_model_id = "01-ai/Yi-34b"
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+# from transformers import AutoModelForCausalLM, AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("01-ai/Yi-34b", use_fast=False)
+
+model = AutoModelForCausalLM.from_pretrained("01-ai/Yi-34b", device_map="auto")
+
+tokenizer = AutoTokenizer.from_pretrained(
+    base_model_id,
+    padding_side="left",
+    add_eos_token=True,
+    add_bos_token=True,
+)
+tokenizer.pad_token = tokenizer.eos_token
+
+def generate_and_tokenize_prompt(prompt):
+    return tokenizer(formatting_func(prompt))
+
+
+tokenized_train_dataset = train_dataset.map(generate_and_tokenize_prompt)
+tokenized_val_dataset = test_dataset.map(generate_and_tokenize_prompt)
+
+import matplotlib.pyplot as plt
+
+def plot_data_lengths(tokenized_train_dataset, tokenized_val_dataset):
+    lengths = [len(x['input_ids']) for x in tokenized_train_dataset]
+    lengths += [len(x['input_ids']) for x in tokenized_val_dataset]
+    print(len(lengths))
+
+    # Plotting the histogram
+    plt.figure(figsize=(10, 6))
+    plt.hist(lengths, bins=20, alpha=0.7, color='blue')
+    plt.xlabel('Length of input_ids')
+    plt.ylabel('Frequency')
+    plt.title('Distribution of Lengths of input_ids')
+    plt.show()
+
+plot_data_lengths(tokenized_train_dataset, tokenized_val_dataset)
+
+
+
+max_length = 512
+
+def generate_and_tokenize_prompt2(prompt):
+    result = tokenizer(
+        formatting_func(prompt),
+        truncation=True,
+        max_length=max_length,
+        padding="max_length",
+    )
+    result["labels"] = result["input_ids"].copy()
+    return result
+
+
+tokenized_train_dataset = train_dataset.map(generate_and_tokenize_prompt2)
+tokenized_val_dataset = test_dataset.map(generate_and_tokenize_prompt2)
+
+
+print(tokenized_train_dataset[1]['input_ids'])
+
+
+plot_data_lengths(tokenized_train_dataset, tokenized_val_dataset)
+
+from peft import prepare_model_for_kbit_training
+
+model.gradient_checkpointing_enable()
+model = prepare_model_for_kbit_training(model)
+
+
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+from peft import LoraConfig, get_peft_model
+
+config = LoraConfig(
+    r=32,
+    lora_alpha=64,
+    target_modules=[
+        "q_proj",
+        "k_proj",
+        "v_proj",
+        "o_proj",
+        "w1",
+        "w2",
+        "w3",
+        "lm_head",
+    ],
+    bias="none",
+    lora_dropout=0.05,
+    task_type="CAUSAL_LM",
+)
+
+model = get_peft_model(model, config)
+print_trainable_parameters(model)
+
+
+
+tokenizer = AutoTokenizer.from_pretrained(
+    base_model_id,
+    padding_side="left",
+    add_eos_token=True,
+    add_bos_token=True,
+)
+tokenizer.pad_token = tokenizer.eos_token
+
+
+if torch.cuda.device_count() > 1: # If more than 1 GPU
+    model.is_parallelizable = True
+    model.model_parallel = True
+
+
+import transformers
+from datetime import datetime
+
+project = "journal-finetune"
+base_model_name = "Yi34brestro"
+run_name = base_model_name + "-" + project
+output_dir = "./" + run_name
+
+trainer = transformers.Trainer(
+    model=model,
+    train_dataset=tokenized_train_dataset,
+    eval_dataset=tokenized_val_dataset,
+    args=transformers.TrainingArguments(
+        output_dir=output_dir,
+        warmup_steps=1,
+        per_device_train_batch_size=2,
+        gradient_accumulation_steps=1,
+        gradient_checkpointing=True,
+        max_steps=300,
+        learning_rate=2.5e-5,
+        fp16=True,
+        optim="paged_adamw_8bit",
+        logging_steps=25,
+        logging_dir="./logs",
+        save_strategy="steps",       # Save the model checkpoint every logging step
+        save_steps=25,                # Save checkpoints every 50 steps
+        evaluation_strategy="steps", # Evaluate the model every logging step
+        eval_steps=25,               # Evaluate and save checkpoints every 50 steps
+        do_eval=True,                # Perform evaluation at the end of training
+        report_to="wandb",           # Comment this out if you don't want to use weights & baises
+        run_name=f"{run_name}-{datetime.now().strftime('%Y-%m-%d-%H-%M')}"          # Name of the W&B run (optional)
+    ),
+    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
+)
+
+model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
+trainer.train()
+
+
+
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+
+base_model_id = "01-ai/Yi-34b"
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+base_model = AutoModelForCausalLM.from_pretrained(
+    base_model_id,
+    quantization_config=bnb_config,
+    device_map="auto",
+    trust_remote_code=True,
+    use_auth_token=True
+)
